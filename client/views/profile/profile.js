1c61a8441cf016e3815a331c0f5322b556f8a052
Joao Bernardo <jb.amaro@hotmail.com>
2015-02-21 11:56:22 +0000

    master - master - Turn profile view universal

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -1,4 +1,14 @@
 Template.profile.helpers({
+  user: function() {
+    var id = Router.current().params.id;
+    var user = Meteor.users.findOne({ _id: id });
+
+    if (!user) {
+      return {};
+    }
+
+    return user;
+  },
   getUsername: function (userId) {
     var user = Meteor.users.findOne(userId);
 
