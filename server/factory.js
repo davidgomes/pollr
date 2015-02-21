aeccbd19f67750380f46b9d697c51f7384868f1c
Gangster Veggies <gangsterveggies@gmail.com>
2015-02-21 02:42:14 +0000

    Refactor user collection

--- server/factory.js
+++ server/factory.js
@@ -1,10 +1,20 @@
 Meteor.startup(function () {
   Questions.remove({});
+  Meteor.users.remove({});
+
+  var user = Meteor.users.findOne({ username: "pedro" });
+
+  if (!user) {
+    Accounts.createUser({ username: "pedro", password: "pedro" });
+  }
+
+  user = Meteor.users.findOne({ username: "pedro" });
 
   if (Questions.find().count() === 0) {
     for (var i = 0; i < 10; i++) {
       var question = {
-        userId: "00",
+        userId: user._id,
+        username: user.username,
         question: "Musica, tema pa " + i.toString(),
         hashtags: ["01", "02"],
         answers: [
