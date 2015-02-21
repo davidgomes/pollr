7c1e44ef3d8c24869c637780ca3a670d641bca39
Joao Bernardo <jb.amaro@hotmail.com>
2015-02-21 15:21:48 +0000

    master - Move follow button to profile page

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -17,5 +17,50 @@ Template.profile.helpers({
     }
     
     return user.username;
+  },
+  toFollow: function (otherId) {
+    if (!Meteor.user() || Meteor.userId() === otherId) {
+      return false;
+    }
+
+    return !_.contains(Meteor.user().followees, otherId);
+  },
+  toUnfollow: function (otherId) {
+    if (!Meteor.user() || Meteor.userId() === otherId) {
+      return false;
+    }
+    
+    return _.contains(Meteor.user().followees, otherId);
   }
 });
+
+Template.profile.events({
+  'click #follow-button': function (event) {
+    event.preventDefault();
+
+    var userID = $('follow-button').attr('data');
+
+    Meteor.call("followUser", userID, function (error) {
+      if (error) {
+        console.log(error);
+        // Display error
+      } else {
+        // Display success message
+      }
+    });
+  },
+  'click #unfollow-button': function (event) {
+    event.preventDefault();
+
+    var userID = $('#unfollow-button').attr('data');
+
+    Meteor.call("unfollowUser", userID, function (error) {
+      if (error) {
+        console.log(error);
+        // Display error
+      } else {
+        // Display success message
+      }
+    });
+  }
+})
