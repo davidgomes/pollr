22eccdc2edfc10f7e45bfc9ea3514f08d73b2d5d
Gangster Veggies <gangsterveggies@gmail.com>
2015-02-21 03:53:32 +0000

    Fix addition logic

--- client/views/new_question/new_question.js
+++ client/views/new_question/new_question.js
@@ -1,12 +1,6 @@
 Template.newQuestion.rendered = function () {
   $('#new-answer-btn').tooltip();
   
-  $('#last-question').keypress(function (e) {
-    if (e.which == 13) {
-      $('#new-question-form').submit();
-    }
-  });
-
   $('input').keypress(function (e) {
     if (e.which == 13) {
       $(this).next().select();
@@ -18,21 +12,17 @@ Template.newQuestion.events({
   'submit #new-question-form': function (e, t) {
     e.preventDefault();
 
-    var question = $('#question').val();
+    var question = $('#new-question-question').val();
     var answers = [];
 
     $.each($('.answer'), function (index, value) {
       answers.push($(value).val());
     });
 
-    Meteor.call('newQuestion', question, answers, function (e, r) {
-      if (e) {
-        console.log(e);
+    Meteor.call('newQuestion', question, answers, function (error) {
+      if (error) {
+        console.log(error);
         // Display error
-      } else {
-        console.log('fadsfds');
-        $('#new-question-form').hide();
-        $('#new-question-button').show();
       }
     });
   }
