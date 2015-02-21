6941d6fc2b6ec2c2313412dbd58a166544b400df
Joao Bernardo <jb.amaro@hotmail.com>
2015-02-21 10:17:54 +0000

    master - Add better in code documentation

--- client/views/new_question/new_question.js
+++ client/views/new_question/new_question.js
@@ -2,23 +2,24 @@ Template.newQuestion.rendered = function () {
   // Add new answer input
   $('#new-answer-btn').click(function() {
     var last = $('#new-question-form').find('#last-question');
+    last.removeAttr('id');
 
     var input = '<input type="text" class="form-control answer pull-left" id="last-question" data="N" placeholder="Option N">';
     var index = parseInt(last.attr('data')) + 1;
     input = input.replace('N', index.toString());
     input = input.replace('N', index.toString());
 
-    console.log(input)
-
-    last.after(input);
-    last.removeAttr('id');
+    // Insert new input
     var padding = $('#new-question-form').css('padding-bottom');
-    var newPadding = parseInt(padding.substring(0, padding.length -2)) + 44;
+    var newPadding = parseInt(padding.substring(0, padding.length - 2)) + 44;
     $('#new-question-form').css('padding-bottom', newPadding);
+    last.after(input);
   })
-  
+ 
+  // Move cursor from input to input
   $('input').keypress(function(e) {
     if (e.which == 13) {
+      console.log('input keypress')
       if ($(this).next().is('button')) {
         $(this).next().next().select();
       } else {
@@ -27,12 +28,15 @@ Template.newQuestion.rendered = function () {
     }
   });
 
+  // Add question enter key
   $('#last-question').keypress(function(e) {
     if (e.which == 13) {
+      console.log('last keypress')
       $('#new-question-form').submit();
     }
   });
-  
+ 
+  // Add question click
   $('#new-question-btn').click(function(e) {
     $('#new-question-form').submit();
   });
